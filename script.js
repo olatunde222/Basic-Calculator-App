2f0fdce4ff232117e4838755631a0a8661d21a90
olatunde222 <olatunde_olukoya@yahoo.com>
2023-12-19 04:51:20 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// JavaScript code
+function addToDisplay(value) {
+  document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+  document.getElementById("display").value = "";
+}
+
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function calculate() {
+  try {
+    const result = eval(document.getElementById("display").value);
+    document.getElementById("display").value = result;
+  } catch (error) {
+    document.getElementById("display").value = "Error";
+  }
+}
